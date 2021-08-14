acfc3d0c8148535de4d5122e113487486e753278
Renato Spakauskas <renatospaka@gmail.com>
2021-08-14 11:14:02 -0300

    feat: :recycle: Expenses List app
    handles the logic of display the filtered expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,22 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css"
 
 const Expenses = (props) => {
   const [year, setYear] = useState("2021");
   const filterYearHandler = thisYear => {setYear(thisYear)};
-  const expenses = props.expenses;
-  const filterExpensesHandler = expenses.filter((item) => {
+
+  const filterExpensesHandler = props.expenses.filter((item) => {
     return item.date.getFullYear().toString() === year
-  });
-  const items = filterExpensesHandler.map((item) => {
-    return (
-      <ExpenseItem 
-        key={item.key}
-        title={item.title} 
-        amount={item.amount} 
-        date={item.date} 
-      />
-    )
   });
   
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={year} onChangeYear={filterYearHandler} />
-        {items}
+        <ExpensesList expenses={filterExpensesHandler} />
       </Card>
     </div>
   )
