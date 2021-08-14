d1a759e0b5e2040e8212ae347333cafa86c289a8
Renato Spakauskas <renatospaka@gmail.com>
2021-08-14 10:12:33 -0300

    feat: :construction_worker: Expenses app
    now it can be filtered by year

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const initialExpenses = [
   { id: "e1", title: "Toilet Paper", amount: 91.87, date: new Date(2021, 6, 23) },
   { id: "e2", title: "New TV", amount: 799.14, date: new Date(2021, 3, 16) },
-  { id: "e3", title: "Car Insurance", amount: 2897.98, date: new Date(2021, 7, 2) },
+  { id: "e3", title: "Car Insurance", amount: 2897.98, date: new Date(2020, 7, 2) },
+  { id: "e4", title: "React Course", amount: 9.98, date: new Date(2020, 2, 22) },
 ]
 
 const App = () => {
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,23 +5,23 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css"
 
 const Expenses = (props) => {
-  const [year, setYear] = useState("2021")
-
-  const expenses = props.expenses
-  const items = expenses.map((item) => {
-    return <ExpenseItem 
-            key={item.key}
-            title={item.title} 
-            amount={item.amount} 
-            date={item.date} 
-          />
+  const [year, setYear] = useState("2021");
+  const filterYearHandler = thisYear => {setYear(thisYear)};
+  const expenses = props.expenses;
+  const filterExpensesHandler = expenses.filter((item) => {
+    return item.date.getFullYear().toString() === year
+  });
+  const items = filterExpensesHandler.map((item) => {
+    return (
+      <ExpenseItem 
+        key={item.key}
+        title={item.title} 
+        amount={item.amount} 
+        date={item.date} 
+      />
+    )
   });
   
-  const filterYearHandler = selectedYear => {
-    console.log("Expenses.js")
-    console.log(selectedYear)
-  }
-
   return (
     <div>
       <Card className="expenses">
